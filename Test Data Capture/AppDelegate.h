7eb5130659651fc2467f048be4263e404e9064ea
masti13harsh <masti13harsh@gmail.com>
2015-04-12 22:58:26 +0530

    Initial Commit

---
+++ Test Data Capture/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test Data Capture
+//
+//  Created by Harsh Sapra on 12/04/15.
+//  Copyright (c) 2015 Harsh Sapra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Test Data Capture/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test Data Capture
+//
+//  Created by Harsh Sapra on 12/04/15.
+//  Copyright (c) 2015 Harsh Sapra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
